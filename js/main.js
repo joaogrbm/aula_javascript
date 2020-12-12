f2891c31ee20847be174d3d20def19ebf6cf38aa
joaogrbm <jgrbm66@gmail.com>
2020-12-12 13:34:45 -0300

    Adicionando main.js

---
+++ js/main.js
@@ -0,0 +1,52 @@
+
+var lista = [“Alemanha”, “Inglaterra”, “Escócia”]/
+lista.push(“Polônia”);
+lista.pop(“Inglaterra”);
+
+
+/*
+var i;
+for(i = 0; i < 5; i++){
+    alert(i);
+}*/
+
+
+/*var count = 0;
+while(count <= 5){
+    console.log(count);
+    count++;
+}*/
+
+
+/*var idade = prompt("Qual tua idade ?");
+if (idade >= 18){
+    alert("Maior de idade");
+}else{
+    alert("Menor de idade");
+};*/
+
+
+
+/*var fruta = {nome:"maça", cor:"vermelha"};
+console.log(fruta.nome);*/
+
+
+
+
+/*var lista = ["maça", "pera", "laranja"];
+lista.push("uva");
+console.log(lista);
+lista.pop();
+console.log(lista);
+console.log(lista.length);
+
+console.log(lista.reverse());
+console.log(lista.toString());*/
+
+
+/*var nome = "João Melo";
+var idade = 26;
+var idade2 = 10;
+alert( nome + " tem "+ idade + " anos ");
+alert(idade + idade2);
+console.log(nome);*/
